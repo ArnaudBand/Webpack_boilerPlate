b8f8164f45c494bac79253c415b6a114bbdccadc
Arnaud <bandonkeyea@gmail.com>
2022-04-27 08:27:33 -0600

    Install dev-server

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   devServer: {
     static: './dist',
@@ -15,9 +16,6 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-  },
-  optimization: {
-    runtimeChunk: 'single',
   },
   module: {
     rules: [
